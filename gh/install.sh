5b389ab4cf65ac870ac06b5fb3bc8022eedd5095
Keith McHugh <kmcq@github.com>
2021-05-09 20:59:16 -0400

    Installs for gh and alacritty

---
+++ gh/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+config_dir=~/.config
+config_file=$config_dir/gh/config.yml
+
+mkdir -p $config_dir
+
+if [ ! -f "$config_file" ]; then
+  echo "No GH cli config found. Symlinking to dotfiles"
+  ln -s ~/.dotfiles/gh "$config_dir"
+fi
