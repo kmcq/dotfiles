91d5248b114e222698d87866783692705c070143
Keith McHugh <kmcq@github.com>
2021-05-14 16:43:32 -0400

    Remove references to ~/.dotfiles

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -2,10 +2,11 @@
 
 config_dir=~/.config
 config_file=$config_dir/alacritty/alacritty.yml
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 mkdir -p $config_dir
 
 if [ ! -f "$config_file" ]; then
   echo "No Alacritty config found. Symlinking to dotfiles"
-  ln -s ~/.dotfiles/alacritty "$config_dir"
+  ln -s "$script_dir/alacritty.yml" "$config_file"
 fi
--- gh/install.sh
+++ gh/install.sh
@@ -2,10 +2,11 @@
 
 config_dir=~/.config
 config_file=$config_dir/gh/config.yml
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 mkdir -p $config_dir
 
 if [ ! -f "$config_file" ]; then
   echo "No GH cli config found. Symlinking to dotfiles"
-  ln -s ~/.dotfiles/gh "$config_dir"
+  ln -s "$script_dir/config.yml" "$config_file"
 fi
