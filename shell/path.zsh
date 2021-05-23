017e552a2d94b380e65ecb68734db679fc10d815
Keith McHugh <kmcq@github.com>
2021-05-23 16:56:49 -0400

    Remove more brew things

--- shell/path.zsh
+++ shell/path.zsh
@@ -2,10 +2,6 @@ case "$(uname -s)" in
   Darwin)
     export PATH="/usr/local/bin:$PATH"
     ;;
-
-  Linux)
-    export PATH="$HOME/.linuxbrew/bin:$PATH"
-    ;;
 esac
 
 export PATH="$ZSH/bin:$PATH"
