adafe9b243a6c4869843458f26e01724cf01b3bf
Keith McHugh <kmcq@github.com>
2021-05-23 16:59:35 -0400

    More more codespaces

--- shell/path.zsh
+++ shell/path.zsh
@@ -2,6 +2,11 @@ case "$(uname -s)" in
   Darwin)
     export PATH="/usr/local/bin:$PATH"
     ;;
+
+  Linux)
+    export PATH="$HOME/.nodenv/bin:$PATH"
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    ;;
 esac
 
 export PATH="$ZSH/bin:$PATH"
