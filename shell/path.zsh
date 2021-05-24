735b1edaf7c15ad116be728104604f1ee0b3356e
Keith McHugh <kmcq@github.com>
2021-05-23 20:41:29 -0400

    More path fixups

--- shell/path.zsh
+++ shell/path.zsh
@@ -12,8 +12,6 @@ esac
 export PATH="$ZSH/bin:$PATH"
 export PATH="./bin:$PATH"
 export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
-export PATH="${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}"
 export PATH="$PATH:$HOME/.yarn/bin"
-export PATH="$PATH:/usr/local/Cellar/node@6/6.9.1/bin"
 export PATH="$PATH:/usr/local/opt/mysql@5.7/bin"
 export PATH="$HOME/go/bin:$PATH"
