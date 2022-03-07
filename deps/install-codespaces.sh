59854e760ab5d2098da72d0d37b967d14cb2da19
Keith McHugh <kmcq@github.com>
2022-03-06 20:09:42 -0500

    Install neovim and diff-so-fancy

--- deps/install-codespaces.sh
+++ deps/install-codespaces.sh
@@ -9,3 +9,12 @@ curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc
+
+# Install neovim
+sudo apt-get install -y libfuse2
+mkdir -p $HOME/.bin
+curl -L -o $HOME/.bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+chmod a+x $HOME/.bin/nvim
+
+# Install diff-so-fancy
+sudo apt-get install -y diff-so-fancy
--- git/install.sh
+++ git/install.sh
@@ -4,7 +4,7 @@ if [ "$(uname -s)" == "Darwin" ]
 then
   echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
   killall gpg-agent
-else
-  # We won't have delta, so just alias it to less
-  alias delta="less"
+# else
+#   # We won't have delta, so just alias it to less
+#   alias delta="less"
 fi
