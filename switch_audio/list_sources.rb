a046eb574e25fcaa8affcd4c1b7a528a16507af2
Keith McHugh <kmcq@github.com>
2022-03-26 16:59:57 -0400

    Exclude zoom weird thing from output options

--- switch_audio/list_sources.rb
+++ switch_audio/list_sources.rb
@@ -1,10 +1,9 @@
 require "json"
 
-sources = `/usr/local/bin/SwitchAudioSource -a -t output -f json`.split("\n").map do |line|
+sources = `/usr/local/bin/SwitchAudioSource -a -t output -f json`.split("\n").reject { |l| l.include?("Zoom") }.map do |line|
   parsed = JSON.parse(line)
   parsed["title"] = parsed["name"]
   parsed["arg"] = parsed["name"]
   parsed
 end
-sources = { items: sources }.to_json
-STDOUT.write(sources)
+STDOUT.write({ items: sources }.to_json)
