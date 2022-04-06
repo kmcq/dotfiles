e405275332bd7a8190681eb76deef8ad55ca53bf
Keith McHugh <kmcq@github.com>
2022-04-06 15:48:29 -0400

    Remove outdated comment

--- git/install.sh
+++ git/install.sh
@@ -4,7 +4,4 @@ if [ "$(uname -s)" == "Darwin" ]
 then
   echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
   killall gpg-agent
-# else
-#   # We won't have delta, so just alias it to less
-#   alias delta="less"
 fi
