9618f4b5131abbd39dc58bc26ecc7ea6d69a30e1
Keith McHugh <kmcq@github.com>
2022-06-20 19:59:57 -0400

    asdf

--- deps/install-mac.sh
+++ deps/install-mac.sh
@@ -14,4 +14,8 @@ brew update
 brew bundle --file="$script_dir/Brewfile"
 brew bundle --file="$script_dir/Brewfile.darwin"
 
+$(brew --prefix)/bin/asdf plugin-list | grep nodejs || $(brew --prefix)/bin/asdf plugin-add nodejs
+$(brew --prefix)/bin/asdf plugin-list | grep ruby || $(brew --prefix)/bin/asdf plugin-add ruby
+asdf install
+
 exit 0
