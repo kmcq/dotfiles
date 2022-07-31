849d9dd9735840d093d630acfd588f8d4bb94bbb
Keith McHugh <kmcq@github.com>
2022-07-31 16:32:42 -0400

    x-platform pinentry install

--- git/install.sh
+++ git/install.sh
@@ -3,6 +3,6 @@
 if [ ! -f ~/.gnupg/gpg-agent.conf ]
 then
   gpg --list-keys > /dev/null
-  echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+  echo "pinentry-program $(which pinentry-mac)" > "$HOME/.gnupg/gpg-agent.conf"
   killall gpg-agent
 fi
