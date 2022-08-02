23435116360ee133a2f9ad80c828038f79bfc2ee
Keith McHugh <kmcq@github.com>
2022-08-02 08:41:53 -0400

    Move git to chezmoi

--- setup-gpg-key.sh
+++ setup-gpg-key.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+gpg --full-generate-key
+
+user ' - What is your GPG signing key? (Run `gpg --list-keys`)'
+read -e gpg_signing_key
+git config --global user.signingkey "$gpg_signing_key"
+
 if [ ! -f ~/.gnupg/gpg-agent.conf ]
 then
   gpg --list-keys > /dev/null
