6d8a50e5fdf9364d24d9918c9f7fe04741039de9
Keith McHugh <kmcq@github.com>
2022-09-26 10:15:23 -0400

    Better lsp errors, don't show 'em all the time

--- dot_config/nvim/lua/conf-lspconfig.lua
+++ dot_config/nvim/lua/conf-lspconfig.lua
@@ -12,11 +12,9 @@ null_ls.setup({
 })
 
 -- Disable inline diagnostics.
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false
-}
-)
+vim.diagnostic.config({
+  virtual_text = false,
+})
 
 -- Add a border to hovers to make them easier to read.
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
