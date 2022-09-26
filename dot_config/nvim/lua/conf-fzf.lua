4d435b47a008ed659d00ffdfd08e5173af7342b6
Keith McHugh <kmcq@github.com>
2022-09-26 10:15:45 -0400

    Add comment

--- dot_config/nvim/lua/conf-fzf.lua
+++ dot_config/nvim/lua/conf-fzf.lua
@@ -9,5 +9,5 @@ vim.keymap.set("n", "\\", ":Ag<space>")
 vim.keymap.set("n", "K", ":Ag <C-R><C-W><CR>")
 
 vim.g.fzf_buffers_jump = 1 -- [Buffers] Jump to the existing window if possible
-vim.g.fzf_preview_window = { "up:40%", "ctrl-\\", }
+vim.g.fzf_preview_window = { "up:40%", "ctrl-\\", } -- ctrl-\\ toggles preview
 vim.g.fzf_layout = { window = { width = 0.8, height = 0.9, yoffset = 0.2 } }
