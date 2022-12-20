5b22eed12b4af0e8f8d7b82461d67749f19109df
Keith McHugh <kmcq@github.com>
2022-12-20 15:16:24 -0800

    Add coauthor command

--- dot_config/nvim/lua/conf-fzf.lua
+++ dot_config/nvim/lua/conf-fzf.lua
@@ -11,3 +11,11 @@ vim.keymap.set("n", "K", ":Ag <C-R><C-W><CR>")
 vim.g.fzf_buffers_jump = 1 -- [Buffers] Jump to the existing window if possible
 vim.g.fzf_preview_window = { "up:40%", "ctrl-\\", } -- ctrl-\\ toggles preview
 vim.g.fzf_layout = { window = { width = 0.8, height = 0.9, yoffset = 0.2 } }
+vim.api.nvim_exec(
+  [[
+    " Replace the default dictionary completion with fzf-based fuzzy completion
+    inoremap <expr> <c-x><c-k> fzf#vim#complete('cat /usr/share/dict/words')
+    inoremap <expr> gco fzf#vim#complete('git coauthors')
+  ]],
+  false
+)
