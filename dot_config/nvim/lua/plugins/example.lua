2a98ce0b351f9efd8191df0e5ad037f4fb9c2f80
Keith McHugh <keith.mchugh@gmail.com>
2023-10-17 11:47:07 -0700

    Add csharp LSP support

--- dot_config/nvim/lua/plugins/example.lua
+++ dot_config/nvim/lua/plugins/example.lua
@@ -156,6 +156,7 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "omnisharp",
         "stylua",
         "shellcheck",
         "shfmt",
@@ -232,6 +233,35 @@ return {
       diagnostics = {
         virtual_text = false,
         float = { border = "rounded" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        omnisharp = {},
+      },
+      -- configure omnisharp to fix the semantic tokens bug (really annoying)
+      setup = {
+        omnisharp = function(_, _)
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "omnisharp" then
+              ---@type string[]
+              local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+              for i, v in ipairs(tokenModifiers) do
+                tokenModifiers[i] = v:gsub(" ", "_")
+              end
+              ---@type string[]
+              local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+              for i, v in ipairs(tokenTypes) do
+                tokenTypes[i] = v:gsub(" ", "_")
+              end
+            end
+          end)
+          return false
+        end,
       },
     },
   },
